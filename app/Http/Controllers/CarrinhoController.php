8cd950ac8232c9010c262f489ba4634d51947d1d
elvishuges <elvishuges@hotmail.com>
2019-01-30 15:50:14 -0300

    finalizando compras e retornando compras finalizadas (json)

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -131,4 +131,68 @@ class CarrinhoController extends Controller
         return redirect()->route('carrinho.index');
     }
 
+
+    public function concluir()
+    {
+        $this->middleware('VerifyCsrfToken');
+
+        $req = Request();
+        $idpedido  = $req->input('pedido_id');
+        $idusuario = Auth::id();
+
+        $check_pedido = Pedido::where([
+            'id'      => $idpedido,
+            'user_id' => $idusuario,
+            'status'  => 'RE' // Reservada
+            ])->exists();
+
+        if( !$check_pedido ) {
+            $req->session()->flash('mensagem-falha', 'Pedido não encontrado!');
+            return redirect()->route('carrinho.index');
+        }
+
+        $check_produtos = PedidoProduto::where([
+            'pedido_id' => $idpedido
+            ])->exists();
+        if(!$check_produtos) {
+            $req->session()->flash('mensagem-falha', 'Produtos do pedido não encontrados!');
+            return redirect()->route('carrinho.index');
+        }
+
+        PedidoProduto::where([
+            'pedido_id' => $idpedido
+            ])->update([
+                'status' => 'PA'
+            ]);
+        Pedido::where([
+                'id' => $idpedido
+            ])->update([
+                'status' => 'PA'
+            ]);
+
+        $req->session()->flash('mensagem-sucesso', 'Compra concluída com sucesso!');
+
+        return redirect()->route('carrinho.compras');
+    }
+
+    public function compras()
+    {
+
+        $compras = Pedido::where([
+            'status'  => 'PA',
+            'user_id' => Auth::id()
+            ])->orderBy('created_at', 'desc')->get();
+        
+        return $compras; // retorna em formato json
+
+        // $cancelados = Pedido::where([
+        //     'status'  => 'CA',
+        //     'user_id' => Auth::id()
+        //     ])->orderBy('updated_at', 'desc')->get();
+
+        // return view('carrinho.compras', compact('compras', 'cancelados'));
+
+        }
+
+
 }
